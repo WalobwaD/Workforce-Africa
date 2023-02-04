d95501a4e330093b8dc9f2888a88851eb98f1fdf
WalobwaD <walobwadan@gmail.com>
2023-02-04 09:01:14 +0300

    Backdrop edit

--- src/components/jobs/JobRequirements.js
+++ src/components/jobs/JobRequirements.js
@@ -8,13 +8,20 @@ const JobRequirements =({requirements})=>(
         alignItems: 'center',
         justifyContent: 'center',
         alignSelf : 'flex-start',
-        marginY: 1
+        marginY: 1,
+        paddingX: 1
 
     }}>
         <CheckCircle sx={{color:'#FD510C', opacity: 0.9}}/>
         <Typography sx={{
+                    marginLeft: 1,
                     opacity: 1,
                     fontWeight: 600,
+                    fontSize: {
+                        mobile: '0.7rem',
+                        tablet: '1rem',
+                        laptop: '1.2rem'
+                    }
         }}>{requirements}</Typography>
     </Box>
 )
